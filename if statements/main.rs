828240b3d6458472a70560f2a9a5648f2dd686fa
Thienan (Teddy) Tran <46636456+thienantran@users.noreply.github.com>
2023-12-19 19:34:26 -0800

    Create main.rs

---
+++ if statements/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    let n = 20;
+
+    if n == 45 {
+        println!("Rust!");
+    }
+    else if n > 50 {
+        println!("It is greater than 50!");
+    }
+    else {
+        println!("It was not 45 and it was less than 50");
+    }
+}
